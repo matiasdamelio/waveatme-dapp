d4bf6b7b5a764447d10fe38ca5e8fab6712e93ae
matiasdamelio <matiasdamelio@gmail.com>
2022-04-17 20:36:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Wave At Me
+
+Buildspace - First Smart Contract
+
+## Environment Variables
+
+To run this project, you vill need to add the following environment variables to your .env file
+
+`STAGING_ALCHEMY_KEY`
+
+`PRIVATE_KEY`
+
+## Usage
+
+To deploy
+
+```
+npx hardhat run scripts/deploy.js --network rinkeby
+```
+
+## Dapp
+
+URL Frontend dapp
+
+```
+https://waveportal-starter-project.matiasdamelio.repl.co/
+```
---
+++ scripts/run.js
@@ -0,0 +1,44 @@
+const main = async () => {
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
+    await waveContract.deployed();
+    
+    console.log("Contract deployed to:", waveContract.address);
+    
+    let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(contractBalance));
+
+    // let waveCount;
+    // waveCount = await waveContract.getTotalWaves();
+    // console.log(waveCount.toNumber());
+    
+    const waveTxn = await waveContract.wave("This is wave #1");
+    await waveTxn.wait();
+
+    const waveTxn2 = await waveContract.wave("This is wave #2");
+    await waveTxn2.wait();
+
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(contractBalance));
+
+    // const [_, randomPerson] = await hre.ethers.getSigners();
+    // waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+    // await waveTxn.wait();
+
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
+
+runMain();
